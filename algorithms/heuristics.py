8a64b64ee8a00ec0196ec77c24894015897ed4d4
Michell Casallas <ma.casallas@uniandes.edu.co>
2026-02-21 20:23:47 -0500

    🐮

--- algorithms/heuristics.py
+++ algorithms/heuristics.py
@@ -16,7 +16,12 @@ def manhattanHeuristic(state, problem):
     The Manhattan distance heuristic.
     """
     # TODO: Add your code here
-    utils.raiseNotDefined()
+    
+    estadoActual = state
+    estadoMeta = problem.goal()
+
+    manhathan = abs(estadoActual[0] - estadoMeta[0]) + abs(estadoActual[1] - estadoMeta[1])
+    return manhathan
 
 
 def euclideanHeuristic(state, problem):
@@ -24,6 +29,10 @@ def euclideanHeuristic(state, problem):
     The Euclidean distance heuristic.
     """
     # TODO: Add your code here
+    estadoInicial = state
+    estadoMeta = problem.goal()
+    euclidiana = ((estadoInicial[0] - estadoMeta[0]) ** 2 + (estadoInicial[1] - estadoMeta[1]) ** 2) ** 0.5
+    return euclidiana
     utils.raiseNotDefined()
 
 
@@ -43,4 +52,44 @@ def survivorHeuristic(state: Tuple[Tuple, Any], problem: MultiSurvivorProblem):
     - Balance heuristic strength vs. computation time (do experiments!)
     """
     # TODO: Add your code here
-    utils.raiseNotDefined()
+    posicion, grid = state
+    sobrevivientes = []
+
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                sobrevivientes.append((i, j))
+
+    if not sobrevivientes:
+        return 0
+
+    distancia_minima = min(abs(posicion[0] - s[0]) + abs(posicion[1] - s[1])
+                           for s in sobrevivientes)
+
+
+    if len(sobrevivientes) == 1:
+        mst_cost = 0
+    else:
+        import heapq
+
+        visited = set()
+        heap = []
+        start = sobrevivientes[0]
+        visited.add(start)
+
+        for s in sobrevivientes[1:]:
+            dist = abs(start[0] - s[0]) + abs(start[1] - s[1])
+            heapq.heappush(heap, (dist, start, s))
+
+        mst_cost = 0
+        while heap and len(visited) < len(sobrevivientes):
+            dist, s1, s2 = heapq.heappop(heap)
+            if s2 not in visited:
+                visited.add(s2)
+                mst_cost += dist
+                for s in sobrevivientes:
+                    if s not in visited:
+                        new_dist = abs(s2[0] - s[0]) + abs(s2[1] - s[1])
+                        heapq.heappush(heap, (new_dist, s2, s))
+
+    return distancia_minima + mst_cost
--- algorithms/search.py
+++ algorithms/search.py
@@ -1,9 +1,18 @@
+from hashlib import new
 from algorithms.problems import SearchProblem
 import algorithms.utils as utils
 from world.game import Directions
 from algorithms.heuristics import nullHeuristic
+import time
 
 
+#Pongo esta vaina para calcular las complejidades jiji gracias chat por tanto 
+def imprimir_resultado(nombre, T_formula, S_formula, nodos_expandidos, memoria_maxima):
+    print(f"\n--- [ANÁLISIS DE {nombre}] ---")
+    print(f"1. Temporal: {T_formula} => {nodos_expandidos} operaciones")
+    print(f"2. Espacial: {S_formula} => {memoria_maxima} nodos en memoria")
+    print("-" * 30)
+    
 def tinyHouseSearch(problem: SearchProblem):
     """
     Returns a sequence of moves that solves tinyHouse. For any other building, the
@@ -32,61 +41,101 @@ def depthFirstSearch(problem: SearchProblem):
     print("Start:", problem.getStartState())
     print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
     print("Start's successors:", problem.getSuccessors(problem.getStartState()))
-    utils.raiseNotDefined()
+    pila = utils.Stack()
+    visitados = set()
+    nodos_expandidos = 0
+    max_memoria = 0
+    
 
+    estadoInicial = problem.getStartState()
+    pila.push((estadoInicial, []))
+
+    while not pila.isEmpty():
+        max_memoria = max(max_memoria, len(visitados) + len(pila.list))
+        estadoActual, camino = pila.pop()
+
+        if problem.isGoalState(estadoActual):
+            imprimir_resultado("DFS", "O(b^m)", "O(b * m)", nodos_expandidos, max_memoria)
+            return camino
+
+        if estadoActual not in visitados:
+            nodos_expandidos += 1
+            visitados.add(estadoActual)
+
+            for sucesor, accion, costo in problem.getSuccessors(estadoActual):
+                pila.push((sucesor, camino + [accion]))
+
+    return []    
+    
+    
 
 def breadthFirstSearch(problem: SearchProblem):
     """
     Search the shallowest nodes in the search tree first.
     """
     # TODO: Add your code here
-    utils.raiseNotDefined()
+    cola = utils.Queue()
+    visitados = set()
+    estadoInicial = problem.getStartState()
+    cola.push((estadoInicial, []))
+    nodos_expandidos = 0
+    max_memoria = 0
+    
+
+    while not cola.isEmpty():
+        max_memoria = max(max_memoria, len(visitados) + len(cola.list))
+        estadoActual, camino = cola.pop()
+
+        if problem.isGoalState(estadoActual):
+            imprimir_resultado("BFS", "O(b^d)", "O(b^d)", nodos_expandidos, max_memoria)
+            return camino
+
+        if estadoActual not in visitados:
+
+            visitados.add(estadoActual)
+            nodos_expandidos += 1
+            for sucesor, accion, costo in problem.getSuccessors(estadoActual):
+                cola.push((sucesor, camino + [accion]))
+
+    return []
 
 
 def uniformCostSearch(problem: SearchProblem):
     """
     Search the node of least total cost first.
     """
-    costo_total = 0
-    camino = []
-    visitados = []
-    cola_prioridad = utils.PriorityQueue()
-    nodo_actual = problem.getStartState()
-    s = Directions.SOUTH
-    n = Directions.NORTH
-    w = Directions.WEST
-    e = Directions.EAST
-    dict_direcciones = {"South":s,"North":n,"West":w,"East":e}
-    
-    while (not problem.isGoalState(nodo_actual)):
-        # Se marca como visitado el nodo en el que se sitúa el robot
-        visitados.append(nodo_actual)
-        # Se obtienen los sucesores del nodo en el que está actualmente el robot
-        sucesores = problem.getSuccessors(nodo_actual)
-        # Añadimos a la cola de prioridad todos los nodos vecinos con prioridad igual a sus pesos (el peso que toma ir a ellos)
-        for sucesor in sucesores:
-            cola_prioridad.push(sucesor,sucesor[2])
-        # Se saca el nodo al que se llega con menor costo y se le asigna al actual
-        mejor_sucesor = cola_prioridad.pop()
+    #variable para el calculo de complejidades
+    inicio_t = time.time()    
+    nodos_expandidos = 0
+    max_memoria = 0
+    estadoInicial = problem.getStartState()
+    colaPrioridad = utils.PriorityQueue()
+    colaPrioridad.push((estadoInicial, [], 0), 0)
+    mejorCosto = {}
+
+    while not colaPrioridad.isEmpty():
         
-        #Si el nodo ya fue visitado se vista el que le sigue
-        while mejor_sucesor[0] not in visitados  and  not cola_prioridad.isEmpty():
-            mejor_sucesor = cola_prioridad.pop()
-        
-        costo_total+=mejor_sucesor[2]
-        camino.append(dict_direcciones[mejor_sucesor[1]])
-        nodo_actual = mejor_sucesor[0]
-        
-        # Vaciamos la cola de prioridad para que en el proximo paso solo tengamos los sucesores del nuevo nodo
-        while not cola_prioridad.isEmpty():
-            elem = cola_prioridad.pop()
-        
-        return camino
-        
-        
-        
-    # TODO: Add your code here
-    utils.raiseNotDefined()
+        memoria_actual = len(mejorCosto) + len(colaPrioridad.heap)
+        if memoria_actual > max_memoria:
+            max_memoria = memoria_actual
+        estadoActual, camino, costoActual = colaPrioridad.pop()
+
+        if estadoActual in mejorCosto and costoActual > mejorCosto[estadoActual]:
+            continue
+
+        mejorCosto[estadoActual] = costoActual
+        nodos_expandidos += 1
+
+        if problem.isGoalState(estadoActual):
+            imprimir_resultado("UCS", "O(b^(C*/ε))", "O(b^(C*/ε))", nodos_expandidos, max_memoria)
+            return camino
+
+        for sucesor, accion, costo in problem.getSuccessors(estadoActual):
+            nuevoCamino = camino + [accion]
+            nuevoCosto = costoActual + costo
+            colaPrioridad.push((sucesor, nuevoCamino, nuevoCosto), nuevoCosto)
+
+    return []
 
 
 def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic):
@@ -94,6 +143,38 @@ def aStarSearch(problem: SearchProblem, heuristic=nullHeuristic):
     Search the node that has the lowest combined cost and heuristic first.
     """
     # TODO: Add your code here
+    tiempoInicio = time.time()
+    nodos_expandidos = 0
+    max_memoria = 0
+    estadoInicial = problem.getStartState()
+    colaPrioridad = utils.PriorityQueue()
+    colaPrioridad.push((estadoInicial, [], 0), heuristic(estadoInicial, problem))
+    mejorCosto = {}
+    nodos_expandidos = 0
+    
+    while not colaPrioridad.isEmpty():
+        estadoActual, camino, costoActual = colaPrioridad.pop()
+
+        if estadoActual in mejorCosto and costoActual > mejorCosto[estadoActual]:
+            continue
+        mejorCosto[estadoActual] = costoActual
+        nodos_expandidos += 1
+        max_memoria = max(max_memoria, len(mejorCosto))
+
+        if problem.isGoalState(estadoActual):
+            imprimir_resultado("A*", "O(b^(C*/ε))", "O(b^(C*/ε))", nodos_expandidos, max_memoria)
+            return camino
+
+        for sucesor, accion, costo in problem.getSuccessors(estadoActual):
+            nuevoCamino = camino + [accion]
+            nuevoCosto = costoActual + costo
+            prioridad = nuevoCosto + heuristic(sucesor, problem)
+            colaPrioridad.push((sucesor, nuevoCamino, nuevoCosto), prioridad)
+    
+    tiempoFin = time.time()
+    print("Tiempo de ejecución: {:.2f} segundos".format(tiempoFin - tiempoInicio), "segundos")
+    return []
+    
     utils.raiseNotDefined()
 
 
